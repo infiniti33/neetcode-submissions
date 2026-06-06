9030609a38058c7a487e42f0ff352c778f5d27a8
Taylor Rodrigues <57120874+infiniti33@users.noreply.github.com>
2026-06-06 12:09:44 -0700

    Add: queue - submission-2

---
+++ Data Structures & Algorithms/queue/submission-2.js
@@ -0,0 +1,90 @@
+class Node {
+    constructor() {
+        this.next = null;
+        this.prev = null;
+        this.val = null;
+    }
+}
+
+class MyDeque {
+    constructor() {
+        this.start = null;
+        this.end = null;
+    }
+
+    /**
+     * @return {boolean}
+     */
+    isEmpty() {
+        return !this.end;
+    }
+
+    /**
+     * @param {number} value
+     */
+    append(value) {
+        if (this.isEmpty()) {
+            this.end = new Node();
+            this.end.val = value;
+            this.start = this.end;
+        } else {
+            this.end.next = new Node();
+            this.end.next.prev = this.end;
+            this.end.next.val = value;
+            this.end = this.end.next;
+        }
+    }
+
+    /**
+     * @param {number} value
+     * @return {void}
+     */
+    appendleft(value) {
+        if (this.isEmpty()) {
+            this.start = new Node();
+            this.start.val = value;
+            this.end = this.start;
+        } else {
+            this.start.prev = new Node();
+            this.start.prev.next = this.start;
+            this.start.prev.val = value;
+            this.start = this.start.prev;
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        if (this.isEmpty()) {
+            return -1;
+        }
+        const end = this.end;
+        if (this.end.prev) {
+            this.end.prev.next = null;
+            this.end = this.end.prev;
+        } else {
+            this.end = null;
+            this.start = null;
+        }
+        return end.val;
+    }
+
+    /**
+     * @return {number}
+     */
+    popleft() {
+        if (this.isEmpty()) {
+            return -1;
+        }
+        const start = this.start;
+        if (this.start.next) {
+            this.start.next.prev = null;
+            this.start = this.start.next;
+        } else {
+            this.start = null;
+            this.end = null;
+        }
+        return start.val;
+    }
+}
