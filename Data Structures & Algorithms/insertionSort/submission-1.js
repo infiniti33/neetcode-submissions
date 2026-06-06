761e0d6e4e599a6b8b5d227e1b2f634fafc718a1
Taylor Rodrigues <57120874+infiniti33@users.noreply.github.com>
2026-06-06 16:57:39 -0700

    Add: insertionSort - submission-1

---
+++ Data Structures & Algorithms/insertionSort/submission-1.js
@@ -0,0 +1,39 @@
+/**
+ * Pair class to store key-value pairs
+ */
+// class Pair {
+//     /**
+//      * @param {number} key The key to be stored in the pair
+//      * @param {string} value The value to be stored in the pair
+//      */
+//     constructor(key, value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+class Solution {
+    /**
+     * @param {Pair[]} pairs
+     * @returns {Pair[][]}
+     */
+    insertionSort(pairs) {
+        if (!pairs.length) {
+            return [];
+        }
+        const output = [[...pairs]];
+
+        for (let i = 1; i < pairs.length; i++) {
+
+            let j = i - 1;
+            while (j >= 0 && pairs[j + 1].key < pairs[j].key) {
+                const temp = pairs[j + 1];
+                pairs[j + 1] = pairs[j]
+                pairs[j] = temp;
+                j--;
+            }
+            output.push([...pairs])
+        }
+
+        return output;
+    }
+}
