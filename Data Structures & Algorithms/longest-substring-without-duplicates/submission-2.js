d8b2e978e998fef971f9de519f6288e7600ad9ac
Taylor Rodrigues <57120874+infiniti33@users.noreply.github.com>
2026-06-06 18:20:40 -0700

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let longest = 0;
+        if (!s.length) {
+            return longest;
+        }
+        let l = 0;
+        let r = 0;
+        let freq = {};
+        while (r < s.length) {
+            freq[s[r]] = (freq[s[r]] || 0) + 1;
+            while (l < r && freq[s[r]] > 1) {
+                freq[s[l]]--;
+                l++;
+            }
+            longest = Math.max(r  - l + 1, longest);
+            r++;
+        }
+
+        return longest;
+    }
+}
