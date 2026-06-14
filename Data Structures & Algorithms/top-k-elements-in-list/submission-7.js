4dc6f0761480eef9264309f9869f005d43e42011
Taylor Rodrigues <57120874+infiniti33@users.noreply.github.com>
2026-06-13 17:50:51 -0700

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const m = new Map();
+        for (const n of nums) {
+            m.set(n, (m.get(n) ?? 0) + 1);
+        }
+        const counts = Array.from({ length: nums.length }, () => []);
+        for (const n of m.keys()) {
+            if (!counts[m.get(n)]) {
+                counts[m.get(n)] = [];
+            }
+            counts[m.get(n)].push(n);
+        }
+        const topK = [];
+        for (let i = counts.length - 1; i >= 0; i--) {
+            while (topK.length < k && counts[i].length) {
+                topK.push(counts[i].pop());
+            }
+            if (topK.length === k) {
+                return topK;
+            }
+        }
+        return topK;
+    }
+}
