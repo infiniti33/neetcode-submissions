7d38316437429b0e7e79c1abc0be237054d8c89e
Taylor Rodrigues <57120874+infiniti33@users.noreply.github.com>
2026-07-11 19:08:49 -0400

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        if (nums.length === 1) {
+            return nums[0];
+        }
+        return Math.max(this.robHouses(nums.slice(1)), this.robHouses(nums.slice(0, nums.length - 1)));
+    }
+
+    robHouses(nums) {
+        let prev1 = nums[0];
+        let prev2 = 0;
+        for (let i = 1; i < nums.length; i++) {
+            const current = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+}
