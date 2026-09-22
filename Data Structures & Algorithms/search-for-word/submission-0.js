118a89d2a4c69febe0f6ba136f816b85274216d4
Taylor Rodrigues <57120874+infiniti33@users.noreply.github.com>
2026-09-21 17:01:50 -0700

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @param {string} word
+     * @return {boolean}
+     */
+    exist(board, word) {
+        const search = (row, col, index = 0, visited = new Set()) => {
+            if (index >= word.length) return true;
+            if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) return false;
+            visited.add(`${row},${col}`);
+            if (board[row][col] === word[index]) {
+                const neighbors = [[row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]];
+                for (const [r, c] of neighbors) {
+                    if (visited.has(`${r},${c}`)) continue;
+                    if (search(r, c, index + 1, visited)) return true;
+                }
+            }
+            visited.delete(`${row},${col}`);
+            return false;
+        }
+
+        for (let row = 0; row < board.length; row++) {
+            for (let col = 0; col < board[row].length; col++) {
+                if (search(row, col)) return true;
+            }
+        }
+
+        return false;
+    }
+}
